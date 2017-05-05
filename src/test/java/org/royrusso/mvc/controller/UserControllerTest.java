b4d667297b3c04f68c8ebda2096e412022a547b4
Andrii Piatakha <andrii.piatakha@truvenhealth.com>
2017-05-05 13:29:43 +0300

    Beans for testing were defined. Mockito version was updated. Mockito rule was implemented.

--- src/test/java/org/royrusso/mvc/controller/UserControllerTest.java
+++ src/test/java/org/royrusso/mvc/controller/UserControllerTest.java
@@ -16,15 +16,17 @@ package org.royrusso.mvc.controller;
  *
  */
 
-
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.royrusso.mvc.domain.User;
 import org.royrusso.mvc.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -37,72 +39,55 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:test-context.xml", "classpath:mockTestBeans.xml"})
+@ContextConfiguration({ "classpath:test-context.xml", "classpath:mock-test-beans.xml" })
 public class UserControllerTest {
 
-    @InjectMocks
-    private UserController userController;
+	@InjectMocks
+	private UserController userController;
 
-    private MockMvc mockMvc;
+	private MockMvc mockMvc;
 
-    @Spy
-    private UserServiceImpl userService;
+	@Rule 
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
 
-    @Before
-    public void setup() {
+	@Spy
+	@Autowired
+	private UserServiceImpl userService;
 
-        // this must be called for the @Mock annotations above to be processed
-        // and for the mock service to be injected into the controller under
-        // test.
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
-    }
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+	}
 
-    @Test
-    public void testGet() throws Exception {
+	@Test
+	public void testGet() throws Exception {
 
-        int userId = 3;
+		int userId = 3;
+		mockMvc.perform(get("/user/" + userId)).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(jsonPath("data.age", is(15)))
+				.andExpect(jsonPath("data.firstName", is("bob"))).andExpect(jsonPath("data.id", is(userId)))
+				.andExpect(jsonPath("success", is(true)));
+	}
 
-        mockMvc.perform(
-                get("/user/" + userId))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("data.age", is(15)))
-                .andExpect(jsonPath("data.firstName", is("bob")))
-                .andExpect(jsonPath("data.id", is(userId)))
-                .andExpect(jsonPath("success", is(true)));
-    }
+	@Test
+	public void testSave() throws Exception {
+		mockMvc.perform(post("/user").contentType(TestUtil.APPLICATION_JSON_UTF8)
+				.content(TestUtil.convertObjectToJsonBytes(new User())))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(jsonPath("success", is(true)));
+	}
 
-    @Test
-    public void testSave() throws Exception {
-        mockMvc.perform(
-                post("/user")
-                        .contentType(TestUtil.APPLICATION_JSON_UTF8)
-                        .content(TestUtil.convertObjectToJsonBytes(new User()))
-        )
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("success", is(true)));
-    }
+	@Test
+	public void testUpdate() throws Exception {
+		mockMvc.perform(put("/user").contentType(TestUtil.APPLICATION_JSON_UTF8)
+				.content(TestUtil.convertObjectToJsonBytes(new User())))
+				.andExpect(MockMvcResultMatchers.status().isOk()).andExpect(jsonPath("success", is(true)));
+	}
 
-    @Test
-    public void testUpdate() throws Exception {
-        mockMvc.perform(
-                put("/user")
-                        .contentType(TestUtil.APPLICATION_JSON_UTF8)
-                        .content(TestUtil.convertObjectToJsonBytes(new User())))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(jsonPath("success", is(true)));
-    }
-
-    @Test
-    public void testDelete() throws Exception {
-        mockMvc.perform(
-                delete("/user/3"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("success", is(true)));
-    }
+	@Test
+	public void testDelete() throws Exception {
+		mockMvc.perform(delete("/user/3")).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON)).andExpect(jsonPath("success", is(true)));
+	}
 }
